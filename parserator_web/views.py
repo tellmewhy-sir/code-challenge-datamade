0fb613659e5b45b6ac8cb9eb34dd982dbba9d789
David <delutilo.dev@gmail.com>
2024-07-22 17:01:46 -0500

    GET request successfully sends back the correct json data and schema

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -1,4 +1,4 @@
-import usaddress
+import usaddress, json
 from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -16,9 +16,23 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        address = request.query_params.get('address')
+        address_components, address_type = self.parse(address)
+        print(address_components, address_type)
+        return Response({
+            'data': {
+                'input_string': address,
+                'address_components': address_components,
+                'address_type': address_type
+            }
+        })
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+
+        address_parse = usaddress.tag(address)
+        address_components = address_parse[0]
+        address_type = address_parse[1]
+
         return address_components, address_type
