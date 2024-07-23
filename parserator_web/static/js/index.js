e311526d4e37b7098d36c31c2f5d8c6e9331ec47
David <delutilo.dev@gmail.com>
2024-07-23 00:16:38 -0500

    form successfully calls API and displays results

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,38 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+$(document).ready(function() {
+   const $form = $('#js-parse-form')
+   const $addressResults = $('#js-address-results')
+
+   $form.on('submit', (e) => {
+      e.preventDefault()
+
+      const address = $('#js-address').val()
+
+      if (!address) {
+      // handle error feedback
+         return
+      }
+
+      fetch(`/api/parse?address=${encodeURIComponent(address)}`)
+      .then(res => res.json())
+      .then((response) => {
+         const { data } = response
+         const { address_components, address_type } = data
+         const resultsTableHtml = Object.keys(address_components).map((tag) => (
+            `
+            <tr>
+               <td>${tag}</td>
+               <td>${address_components[tag]}</td>
+            </tr>
+            `
+         )).join('')
+         $('#js-results-table').html(resultsTableHtml)
+         $('#js-parse-type').text(address_type)
+         $addressResults.show()
+      })
+      .catch((err) => {
+
+      })
+   })
+})
